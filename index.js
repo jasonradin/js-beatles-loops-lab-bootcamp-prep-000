5bde754beb3b3f963eaf87e52fb3dbfae5c510b4
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-10 15:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(arrayM, arrayI) {
   var arr = []; 
-  for(var i = 0, i < arrayM.length; i += 1) {
+  for(var i = 0, i  arrayM.length; i += 1) {
     arr.push(`${arrayM[i]} plays ${arrayI[i]}`)
   }
   return arr; 
